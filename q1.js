49b190010aaf2c4c8e46507b584fc8668d486b35
mandibchaulagain <mandibchaulagain8@gmail.com>
2024-04-28 20:14:10 +0545

    did some array problems for day 1

--- q1.js
+++ q1.js
@@ -5,31 +5,65 @@ for(let i=0; i<=10;i+=2){
 
 //Print a table containing multiplication tables
 
-var x= 2;
+{
+    var x= 2;
 
-for(let i=1; i<=10; i++){
-    console.log(`${x} x ${i} = ${x*i}`);
+    for(let i=1; i<=10; i++){
+        console.log(`${x} x ${i} = ${x*i}`);
+    }
 }
 
 //Create a length converter function
 
-var km = 1;
-function kmtomiles(km){
-    miles = km*0.621371;
-    console.log(`${km} Kilometers is ${miles} miles.`)
-}
+{
+    var km = 1;
+    function kmtomiles(km){
+        miles = km*0.621371;
+        console.log(`${km} Kilometers is ${miles} miles.`)
+    }
+
 kmtomiles(km)
+}
 
 //Calculate the sum of numbers within an array
-let array3 = []
-function addtwoarrays(array1,array2){
-    
-    for(i = 0; i<array1.length && array2.length; i+=1){
-        array3.push(array1[i]+array2[i]);
+
+{
+    let array3 = []
+    function addtwoarrays(array1,array2){
         
+        for(i = 0; i<array1.length && array2.length; i+=1){
+            array3.push(array1[i]+array2[i]);
+            
+        }
     }
+    addtwoarrays([1,2,3],[4,5,6])
+    console.log(array3);
 }
-addtwoarrays([1,2,3],[4,5,6])
-console.log(array3);
 
+//Create a function that gives a reversed array using two arrays
+{
+    let array3 = [],key;
+    let reverseArray = (array1) =>{
+        for(let i = array1.length-1; i>=0; i-=1){
+            key = array1[i];
+            array3.push(key);
+        }
+    }
+    reverseArray([1,2,3,4,5])
+    console.log(array3)
+}
 
+// Create a function that reverses an array using only one array
+{
+    let array1 = [1, 2, 3, 4, 5, 6,7];
+    let reverseArray = (array1) => {
+        for (let i = 0; i < array1.length / 2; i++) {
+            let temp = array1[i];
+            array1[i] = array1[array1.length - 1 - i];
+            array1[array1.length - 1 - i] = temp;
+        }
+    };
+    reverseArray(array1);
+    console.log(array1);
+    
+}
