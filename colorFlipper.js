03afc00f7d174ff8fa2b2d64e3583485750bcf46
mandibchaulagain <mandibchaulagain8@gmail.com>
2024-05-04 20:36:20 +0545

    day 7. used array to give bg color instead of giving direct value

--- colorFlipper.js
+++ colorFlipper.js
@@ -1,7 +1,8 @@
 const clickMeButton = document.getElementById('clickMeButton');
 
-
+const arrayColor = ["red","blue","yellow"];
+const arrayValue = Math.floor(Math.random()*arrayColor.length);
 function clicksWork(){
-    document.querySelector("#cf1.colorFlipper").style.backgroundColor = "red";
+    document.querySelector("#cf1.colorFlipper").style.backgroundColor = arrayColor[arrayValue];
 }
 clickMeButton.addEventListener('click',clicksWork);
