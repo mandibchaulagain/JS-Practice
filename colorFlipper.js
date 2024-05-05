6fb975acbb74ebf42df29e1a337c16cf0ed1a863
mandibchaulagain <mandibchaulagain8@gmail.com>
2024-05-05 19:19:05 +0545

    day 8. made it possible to select divs to get access to different range of colors

--- colorFlipper.js
+++ colorFlipper.js
@@ -3,12 +3,38 @@ const arrayColor = ["red","blue","yellow"];
 
 var currentColor = -1;
 var arrayValue;
-function clicksWork(){
-    do{
-        arrayValue = Math.floor(Math.random()*arrayColor.length);
-    }while(currentColor=== arrayValue);
-    document.querySelector("#cf1.colorFlipper").style.backgroundColor = arrayColor[arrayValue];
-    document.getElementById("bg1").innerHTML = arrayColor[arrayValue];
-    currentColor = arrayValue;
+
+
+//for the two divs
+const selectableDivs = document.querySelectorAll('.colorDiv');
+
+function handleDivClick(event){
+    
+    switch(event.currentTarget.id) {
+        case 'colorFlipper1':
+            fixArr();
+            break;
+        case 'colorFlipper2':
+            infiniteHex();
+            break;
+        default:
+            break;
+    }
 }
-clickMeButton.addEventListener('click',clicksWork);
+
+
+selectableDivs.forEach(div => {
+    div.addEventListener('click', handleDivClick);
+});
+
+function fixArr(){
+    function clicksWork(){
+        do{
+            arrayValue = Math.floor(Math.random()*arrayColor.length);
+        }while(currentColor=== arrayValue);
+        document.querySelector("#cf1.colorFlipper").style.backgroundColor = arrayColor[arrayValue];
+        document.getElementById("bg1").innerHTML = arrayColor[arrayValue];
+        currentColor = arrayValue;
+    }
+    clickMeButton.addEventListener('click',clicksWork);
+}
